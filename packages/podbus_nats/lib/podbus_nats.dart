6f79842445e7d5a32ef891463ce5b1bdd798d524
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-07-12 14:09:39 +0500

    Export JetStream heartbeat helper

--- packages/podbus_nats/lib/podbus_nats.dart
+++ packages/podbus_nats/lib/podbus_nats.dart
@@ -1,4 +1,5 @@
 export 'src/config.dart';
+export 'src/jetstream_heartbeat.dart';
 export 'src/nats_client_adapter.dart';
 export 'src/nats_jetstream_job_queue.dart';
 export 'src/nats_message_bus.dart';
